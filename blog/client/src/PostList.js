6e4f19322db7f36f58e911eb11753c1a310cc816
Norman Skinner <norman.skinner@live.com>
2020-05-04 11:39:52 -0700

    Added a direct gitignore and made progress on project.

--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get(`http://localhost:4000/posts`);
 
     setPosts(res.data);
   };
@@ -23,6 +25,8 @@ export default () => {
       >
         <div className="catd-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
