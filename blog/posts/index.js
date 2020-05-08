0ac2a97da6b4bc2c234720ae9cdc1f8a5a68216f
Norman Skinner <norman.skinner@live.com>
2020-05-08 14:49:24 -0700

    Wrapped up moderator. Need to test.

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -35,7 +35,7 @@ app.post('/posts', async (req, res) => {
 app.post('/events', (req, res) => {
     console.log('Received Event ', req.body.type);
 
-    res.send({});
+    res.send({ status: 'OK' });
 });
 
 app.listen(4000, () => {
